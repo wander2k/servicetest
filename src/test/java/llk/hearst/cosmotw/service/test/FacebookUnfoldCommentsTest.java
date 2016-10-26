8a42441f2e3a614d1abf34322d7a12404e2ed261
wander2k <lilingkun@gmail.com>
2016-10-26 17:41:28 +0900

    Refactoring.

--- src/test/java/llk/hearst/cosmotw/service/test/FacebookUnfoldCommentsTest.java
+++ src/test/java/llk/hearst/cosmotw/service/test/FacebookUnfoldCommentsTest.java
@@ -17,92 +17,98 @@ import junit.framework.TestCase;
 
 // https://thetower.atlassian.net/browse/WOLVERINE-784
 public class FacebookUnfoldCommentsTest extends TestCase {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  public void setUp() throws Exception {
-    FirefoxProfile profile = new FirefoxProfile();
-    profile.setPreference("network.http.phishy-userpass-length", 255);
-    driver = new FirefoxDriver(profile);
+	public void setUp() throws Exception {
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("network.http.phishy-userpass-length", 255);
+		driver = new FirefoxDriver(profile);
 
-    baseUrl = "http://lingkun.li:Llk228510@cosmotw.hmitech.io/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+		baseUrl = "http://lingkun.li:Llk228510@cosmotw.hmitech.io";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  public void testCosmotwIoFBUnfold() throws Exception {
-    driver.get(baseUrl + "/");
+	public void testCosmotwIoFBUnfold() throws Exception {
+		driver.get(baseUrl + "/");
 
-    //WebDriverWait wait = new WebDriverWait(driver, 10);
-//    wait.until(ExpectedConditions.alertIsPresent());
-//
-//
-//    Credentials
-//    Alert
-    // wait.until(ExpectedConditions.alertIsPresent());
-    //alert.authenticateUsing(new UserAndPassword(**username**, **password**));
+		// WebDriverWait wait = new WebDriverWait(driver, 10);
+		// wait.until(ExpectedConditions.alertIsPresent());
+		//
+		//
+		// Credentials
+		// Alert
+		// wait.until(ExpectedConditions.alertIsPresent());
+		// alert.authenticateUsing(new UserAndPassword(**username**,
+		// **password**));
 
-    // ERROR: Caught exception [ERROR: Unsupported command [createCookie | nsghtn=dev | ]]
-    driver.get(baseUrl + "/");
+		// ERROR: Caught exception [ERROR: Unsupported command [createCookie |
+		// nsghtn=dev | ]]
 
-    WebDriverWait wait = new WebDriverWait(driver, 10);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("homepage")));
-    //wait.wait(5);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("homepage")));
 
-    Cookie cookie = new Cookie("nsghtn", "dev");
-    driver.manage().addCookie(cookie);
+		driver.get(baseUrl + "/");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("homepage")));
 
+		Cookie cookie = new Cookie("nsghtn", "dev");
+		driver.manage().addCookie(cookie);
 
-    driver.findElement(By.xpath("//a[contains(text(),'25歲前要養出讓男人對妳著迷的好習慣 姐立志當個不膩的女人')]")).click();
-    assertEquals("dev", driver.manage().getCookieNamed("nsghtn").getValue());
-    // ERROR: Caught exception [ERROR: Unsupported command [getCookieByName | nsghtn | ]]
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("article")));
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("comments-container--standard-article")));
+		driver.findElement(By.xpath("//a[contains(text(),'25歲前要養出讓男人對妳著迷的好習慣 姐立志當個不膩的女人')]")).click();
+		assertEquals("dev", driver.manage().getCookieNamed("nsghtn").getValue());
+		// ERROR: Caught exception [ERROR: Unsupported command [getCookieByName
+		// | nsghtn | ]]
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("article")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("comments-container--standard-article")));
 
-    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("button.module-comments--toggle"), "隱藏留言"));
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("button.module-comments--toggle"),
+				"隱藏留言"));
 
-    //assertEquals(, driver.findElement().getText());
-  }
+		// assertEquals(, driver.findElement().getText());
+	}
 
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	public void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
